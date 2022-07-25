d44f643394abc1a3eef9601554819e2c4669d34a
Cynthia Black <cindycblack@gmail.com>
2022-07-25 15:02:01 -0800

    Fetch works, images in DOM

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,12 +7,19 @@ document.querySelector('#opportunity').addEventListener('click', getOpportunity)
 // Get the images associated with each rover
 function getCuriosity() {
     const userDate = document.querySelector('input').value;
-    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${userDate}&api_key=DBwjcbbJSpj4f7gY88ljgf2HJKeD0nC8wGLqBGdF`;
+    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${userDate}&api_key=DEMO_KEY`;
 
     fetch(url)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-      console.log(data)
+      if (data.photos.length === 0) {
+        document.querySelector('#search-result-text').innerText = 'No results. Please try a different date.'
+      }
+      
+      else if (data.photos.length > 0) {
+        document.querySelector('#imageOne').src = data.photos[0].img_src;
+        document.querySelector('#imageTwo').src = data.photos[1].img_src;
+        }
     })
     .catch(err => {
         console.log(`error ${err}`)
@@ -20,34 +27,43 @@ function getCuriosity() {
 }
 
 function getSpirit() {
-  // Grab user input
+  const userDate = document.querySelector('input').value;
   const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?earth_date=${userDate}&api_key=DEMO_KEY`;
-  console.log(`Spirit ${url}`)
+
+  fetch(url)
+  .then(res => res.json()) // parse response as JSON
+  .then(data => {
+    if (data.photos.length === 0) {
+      document.querySelector('#search-result-text').innerText = 'No results. Please try a different date.'
+    }
+    
+    else if (data.photos.length > 0) {
+      document.querySelector('#imageOne').src = data.photos[0].img_src;
+      document.querySelector('#imageTwo').src = data.photos[1].img_src;
+      }
+  })
+  .catch(err => {
+      console.log(`error ${err}`)
+  });
 }
 
 function getOpportunity() {
+  const userDate = document.querySelector('input').value;
   const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?earth_date=${userDate}&api_key=DEMO_KEY`;
-  console.log(`Opportunity ${url}`)
-}
-/*
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = `https://api.nasa.gov/planetary/apod?api_key=DBwjcbbJSpj4f7gY88ljgf2HJKeD0nC8wGLqBGdF&date=${choice}`
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-        if (data.media_type == 'image') {
-          document.querySelector('img').src = data.hdurl
-        }
-        else if (data.media_type == 'video') {
-          document.querySelector('iframe').src = data.url
-        }
-        document.querySelector('#explanation').innerText = data.explanation
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+  .then(res => res.json()) // parse response as JSON
+  .then(data => {
+    if (data.photos.length === 0) {
+      document.querySelector('#search-result-text').innerText = 'No results. Please try a different date.'
+    }
+    
+    else if (data.photos.length > 0) {
+      document.querySelector('#imageOne').src = data.photos[0].img_src;
+      document.querySelector('#imageTwo').src = data.photos[1].img_src;
+      }
+  })
+  .catch(err => {
+      console.log(`error ${err}`)
+  });
 }
-*/
